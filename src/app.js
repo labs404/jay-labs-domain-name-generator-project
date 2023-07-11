bed846f76a77da93a5e2d05f62ca1078c00d07c4
labs404 <136141267+labs404@users.noreply.github.com>
2023-07-11 00:30:56 +0000

    console and document list names

--- src/app.js
+++ src/app.js
@@ -6,19 +6,31 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  let pronoun = ["the", "our"];
-  let adj = ["great", "big"];
-  let noun = ["jogger", "racoon"];
-  let extension = [".com", ".net", ".us", ".io"];
+  let domain = generateDomainName();
+  let domainElement = document.getElementById("domain");
+  // domainElement.innerHTML = domain; // not necessary when generating new 'p' elements
+};
 
-  for (let i = 0; i < pronoun.length; i++) {
-    for (let j = 0; j < adj.length; j++) {
-      for (let k = 0; k < noun.length; k++) {
-        for (let l = 0; l < extension.length; l++) {
-          let domain = pronoun[i] + adj[j] + noun[k] + extension[l];
-          console.log(domain);
+let firstPart = ["richards-", "gilfoyles-", "dineshs-", "jareds-"];
+let secondPart = ["serverdocs.", "cloudstorage.", "app.", "codespace."];
+let thirdPart = ["hooli", "aviato", "pied-piper"];
+let fourthPart = [".com", ".net", ".org"];
+
+function generateDomainName() {
+  for (let i = 0; i < firstPart.length; i++) {
+    for (let j = 0; j < secondPart.length; j++) {
+      for (let k = 0; k < thirdPart.length; k++) {
+        for (let l = 0; l < fourthPart.length; l++) {
+          let domainString =
+            firstPart[i] + secondPart[j] + thirdPart[k] + fourthPart[l];
+          const para = document.createElement("p");
+          const node = document.createTextNode(domainString);
+          para.appendChild(node);
+          const element = document.getElementById("div1");
+          element.appendChild(para);
+          console.log(domainString);
         }
       }
     }
   }
-};
+}
