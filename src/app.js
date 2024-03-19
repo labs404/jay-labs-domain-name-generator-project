41770cc1d94949ef5ab2e424c8359f959113e446
Jay Labs <136141267+labs404@users.noreply.github.com>
2024-03-19 00:14:57 +0000

    refactored

--- src/app.js
+++ src/app.js
@@ -12,9 +12,14 @@ function generateDomainName() {
         for (let l = 0; l < fourthPart.length; l++) {
           let domainString =
             firstPart[i] + secondPart[j] + thirdPart[k] + fourthPart[l];
+          //create an empty html paragraph element.
           const paragraph = document.createElement("p");
+
+          //inject the domainString into the empty paragraph element.
           const content = document.createTextNode(domainString);
           paragraph.appendChild(content);
+
+          //append the generated paragraph into the div1 element in the body of index.html
           const element = document.getElementById("div1");
           element.appendChild(paragraph);
           console.log(domainString);
